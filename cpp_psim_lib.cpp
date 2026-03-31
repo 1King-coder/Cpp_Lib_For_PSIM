10b1b4791e2cf1486b1a8c4da899d7fc3482c1de
Vitor dos Santos Barcelos <progvitor@gmail.com>
2026-03-31 14:33:05 -0300

    tarefa 2

--- cpp_psim_lib.cpp
+++ cpp_psim_lib.cpp
@@ -4,7 +4,30 @@
 #include <math.h>
 #include "cpp_psim_lib.h"
 
+double y_1 = 0;
+double y_2 = 0;
+double x_1 = 0;
+double x_2 = 0;
+
+
+double B[3] = {
+    9.8696e-06, 1.97392e-05, 9.8696e-06
+};
+double A[3] = {
+    4.0062598696, -7.9999802608, 3.9937598696
+};
+
 extern "C" {
+    void calcOut( double* in, double* out) {
+        double x = *in;
+
+        *out = (B[0] * x + B[1]*x_1 + B[2]*x_2 - A[1]*y_1 - A[2]*y_2)/A[0];
+        y_2 = y_1;
+        y_1 = *out;
+        x_2 = x_1;
+        x_1 = x;
+    }
+
     void SimulationStep(
             double t,
             double delt,
@@ -16,77 +39,11 @@ extern "C" {
             int nThreadIndex,
             void * pAppPtr
     ) {
-        HALL_INPUTS hallInputs = {0, 0, 0};
-        hallInputs.HallA = static_cast<uint8_t>(in[0]);
-        hallInputs.HallB = static_cast<uint8_t>(in[1]);
-        hallInputs.HallC = static_cast<uint8_t>(in[2]);
-
-        INVERSOR_SWITCHES bldcSwitches = {
-            0,0,0,0,0,0
-        };
-
-        bldcHallSensorComLogic(&bldcSwitches, hallInputs, CLOCKWISE);
-
-        out[0] = bldcSwitches.Sa_top;
-        out[1] = bldcSwitches.Sa_bot;
-        out[2] = bldcSwitches.Sb_top;
-        out[3] = bldcSwitches.Sb_bot;
-        out[4] = bldcSwitches.Sc_top;
-        out[5] = bldcSwitches.Sc_bot;
-
-
+        calcOut(&in[0], &out[0]);
         *pnError = 0; //Success
     }
 
-    void bldcHallSensorComLogic(
-        INVERSOR_SWITCHES* switches,
-        const HALL_INPUTS hallInputs,
-        const Rotation_Direction direction
-    ) {
-        uint8_t hallIn = pow(2, 2) * hallInputs.HallA + pow(2, 1) * hallInputs.HallB + hallInputs.HallC;
-        switch (direction) {
 
-            case CLOCKWISE: {
-                if (hallIn == 4) {
-                    switches->Sa_top = 1;
-                    switches->Sc_bot = 1;
-                }
-                if (hallIn == 6) {
-                    switches->Sa_top = 1;
-                    switches->Sb_bot = 1;
-                }
-
-                if (hallIn == 2) {
-                    switches->Sb_top = 1;
-                    switches->Sa_bot = 1;
-                }
-                if (hallIn == 3) {
-                    switches->Sb_top = 1;
-                    switches->Sc_bot = 1;
-                }
-
-                if (hallIn == 1) {
-                    switches->Sc_top = 1;
-                    switches->Sb_bot = 1;
-                }
-                if (hallIn == 5) {
-                    switches->Sc_top = 1;
-                    switches->Sa_bot = 1;
-                }
-                break;
-            }
-
-
-            case COUNTER_CLOCKWISE: {
-
-                break;
-            }
-
-        }
-
-
-
-    }
 
 }
 
--- cpp_psim_lib.h
+++ cpp_psim_lib.h
@@ -9,32 +9,7 @@
 #endif
 #include <cstdint>
 extern "C" {
-    typedef struct {
-        uint8_t Sa_top;
-        uint8_t Sa_bot;
-        uint8_t Sb_top;
-        uint8_t Sb_bot;
-        uint8_t Sc_top;
-        uint8_t Sc_bot;
-    } INVERSOR_SWITCHES;
-
-    typedef struct {
-        uint8_t HallA;
-        uint8_t HallB;
-        uint8_t HallC;
-    } HALL_INPUTS;
-
-    enum Rotation_Direction {
-        CLOCKWISE,
-        COUNTER_CLOCKWISE,
-    };
-
-
-    void bldcHallSensorComLogic (
-        INVERSOR_SWITCHES* switches,
-        HALL_INPUTS hallInputs,
-        Rotation_Direction direction
-    );
+    void calcOut(double *in, double *out);
 
 }
 
